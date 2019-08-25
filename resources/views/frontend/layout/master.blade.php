c598fba6571d1c2c9470aa9588803f26cf37f319
Lap Dam <xuanlap93@gmail.com>
2019-08-25 16:23:54 +0700

    fix: change messager tool

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -15,23 +15,32 @@
         @yield('meta')
     </head>
     <body>
-        <script>
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '448249385342778',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.1'
-    });
-  };
+<!-- Load Facebook SDK for JavaScript -->
+      <div id="fb-root"></div>
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v4.0'
+          });
+        };
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "https://connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+      <!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="367827510084685"
+  theme_color="#0084ff"
+  logged_in_greeting="CHUYÊN CUNG CẤP CÁC LOẠI THIẾT BỊ NHÀ BẾP NHẬP KHẨU"
+  logged_out_greeting="CHUYÊN CUNG CẤP CÁC LOẠI THIẾT BỊ NHÀ BẾP NHẬP KHẨU">
+</div>
 </script>
         <div class="container">
             {{ csrf_field() }}
@@ -50,29 +59,3 @@
         <script src="/frontend/js/order.js?v=<?= Config::get('app.version'); ?>" type="text/javascript"></script>
     </body>
 </html>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.1&appId=448249385342778&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-<!-- Load Facebook SDK for JavaScript -->
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js#xfbml=1&version=v2.12&autoLogAppEvents=1';
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-
-<!-- Your customer chat code -->
-<div class="fb-customerchat"
-  attribution=setup_tool
-  page_id="367827510084685"
-  theme_color="#0084ff"
-  logged_in_greeting="CHUYÊN CUNG CẤP CÁC LOẠI THIẾT BỊ NHÀ BẾP NHẬP KHẨU"
-  logged_out_greeting="CHUYÊN CUNG CẤP CÁC LOẠI THIẾT BỊ NHÀ BẾP NHẬP KHẨU">
-</div>
